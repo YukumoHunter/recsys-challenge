57d8b93d5614272482fd39171b3e5baa7e0deb44
Gijs de Jong <berichtaangijs@gmail.com>
2024-06-26 21:38:29 +0200

    Port title encoder from old repo

--- src/recsys_challenge/model/__init__.py
+++ src/recsys_challenge/model/__init__.py
@@ -1,9 +1,11 @@
 from ._gerl import GERLModel
 from ._modules import SelfAttention, MultiHeadAttention, create_mask_from_len_for_seqs
+from ._title_encoder import TitleEncoder
 
 __all__ = [
     GERLModel,
     SelfAttention,
     MultiHeadAttention,
-    create_mask_from_len_for_seqs
+    create_mask_from_len_for_seqs,
+    TitleEncoder,
 ]
